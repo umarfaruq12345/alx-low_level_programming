75e24301be3313e2135ec32057018b9e53d56067
umarfaruq12345 <faruqoyetayo@gmail.com>
2023-07-27 16:35:09 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *rot13 - encode string using the rot13 encoding scheme
+ *@s: string to be encoded
+ *Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; a[j] != '\0' ; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
