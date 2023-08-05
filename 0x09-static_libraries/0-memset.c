8535045f5f3075d745299c085d3d62573bb32a5a
umarfaruq12345 <136034515+umarfaruq12345@users.noreply.github.com>
2023-08-05 10:13:15 -0700

    0-memset.c

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills memory with a constant byte
+ *
+ * @s: input pointer to char type represents the pointer
+ *     to the block of memory to fill
+ *
+ * @b: input variables of char type represents the character to fill
+ *
+ * @n: unsigned int variable the number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory area @s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
