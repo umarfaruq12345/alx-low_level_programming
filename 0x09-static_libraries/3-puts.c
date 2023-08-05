70b6d924aa4028fd27875e3d973a6c405b8f8587
umarfaruq12345 <136034515+umarfaruq12345@users.noreply.github.com>
2023-08-05 11:25:21 -0700

    3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string to be printed
+ *
+ * Return: return 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
+}
